bd45dac741bcc5be82deb7d190d0fe8e228217b7
Thomas Malowanczyk <tkm.delegate@hotmail.com>
2018-04-03 12:17:46 +0000

    Updated build script to handle new use of chocolatey

--- src/build.cmd
+++ src/build.cmd
@@ -1,7 +1,14 @@
 @echo off
 cls
+
+WHERE nuget
+if errorlevel 1 (
+  echo nuget not installed. Please install with Chocolatey
+  exit /b %errorlevel%
+)
+
 if not exist packages\FAKE\tools\Fake.exe (
-  .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  nuget install FAKE -OutputDirectory packages -ExcludeVersion
 )
 packages\FAKE\tools\FAKE.exe build.fsx %*
 pause
