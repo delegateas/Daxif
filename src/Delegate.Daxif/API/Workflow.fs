bb04196224f7dab11a6e57cd7616517f8a59dfe1
Fernando Dominguez <fdominguez@cenergistic.com>
2019-03-13 13:47:12 -0500

    Added isolatiuon mode option to workflow registration

--- src/Delegate.Daxif/API/Workflow.fs
+++ src/Delegate.Daxif/API/Workflow.fs
@@ -2,15 +2,19 @@
 
 open DG.Daxif.Common
 open DG.Daxif.Modules.Workflow
-
+open DG.Daxif.Modules.Plugin.Domain
 open Utility
 open InternalUtility
+
 
 type Workflow private () =
 
   /// <summary>Synchronizes all CodeActivities found in your local assembly to CRM.</summary>
   /// <param name="env">Environment the action should be performed against.</param>
-  static member Sync(env: Environment, assemblyPath: string, solutionName: string, ?logLevel: LogLevel) =
+  static member Sync(env: Environment, assemblyPath: string, solutionName: string, ?isolationMode: PluginIsolationMode, ?logLevel: LogLevel) =
     let proxyGen = env.connect(log).GetProxy
     log.setLevelOption logLevel
-    Main.syncSolution proxyGen solutionName assemblyPath
+
+    let isolationMode = isolationMode ?| PluginIsolationMode.Sandbox
+
+    Main.syncSolution proxyGen solutionName assemblyPath isolationMode
