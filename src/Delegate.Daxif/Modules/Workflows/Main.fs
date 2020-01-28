e3b1cfce0f2c6d62b84f8389b4b06f20714a2759
Thomas Kjærgaard Malowanczyk <tkm@delegate.dk>
2020-01-28 09:44:51 +0100

    Fix minor string issue

--- src/Delegate.Daxif/Modules/Workflows/Main.fs
+++ src/Delegate.Daxif/Modules/Workflows/Main.fs
@@ -1,10 +1,5 @@
 ﻿module DG.Daxif.Modules.Workflow.Main
 
-open DG.Daxif
-open DG.Daxif.Common
-open DG.Daxif.Modules
-open DG.Daxif.Common
-open DG.Daxif.Common.Utility
 open DG.Daxif.Common.InternalUtility
 
 let syncSolution proxyGen solution dll isolationMode = 
@@ -12,4 +7,4 @@ let syncSolution proxyGen solution dll isolationMode =
   log.Verbose @"Solution: %s" solution
   log.Verbose @"Path to Workflow Assembly: %s" dll
   WorkflowsHelper.syncSolution' proxyGen solution dll isolationMode
-  log.Info @"The solution Workflow were synced successfully"
+  log.Info "The solution Workflow were synced successfully"
