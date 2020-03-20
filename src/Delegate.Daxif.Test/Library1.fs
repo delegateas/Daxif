6678b0fa3aa3eb1022b047fc7a7e695bb52e39e1
Thomas Kjærgaard Malowanczyk <tkm@delegate.dk>
2020-03-20 14:34:06 +0100

    Initial setup of daxif test project

---
+++ src/Delegate.Daxif.Test/Library1.fs
@@ -0,0 +1,51 @@
+﻿module Delegate.Daxif.Test.Example
+
+open Xunit
+open System
+open Microsoft.Crm.Sdk
+open Microsoft.Xrm.Sdk
+open Microsoft.Xrm.Sdk.Client
+open Microsoft.Xrm.Sdk.Messages
+open Microsoft.Xrm.Sdk.Metadata
+open Microsoft.Xrm.Sdk.Query
+open DG.Daxif
+open DG.Daxif.Modules.Solution
+
+//type testEnvironment = 
+
+type TestOrg () = 
+
+  interface IOrganizationService with
+    member this.Associate(entityName: string, entityId: Guid, relationship: Relationship, relatedEntities: EntityReferenceCollection): unit = 
+      raise (System.NotImplementedException())
+    member this.Create(entity: Entity): Guid = 
+      raise (System.NotImplementedException())
+    member this.Delete(entityName: string, id: Guid): unit = 
+      raise (System.NotImplementedException())
+    member this.Disassociate(entityName: string, entityId: Guid, relationship: Relationship, relatedEntities: EntityReferenceCollection): unit = 
+      raise (System.NotImplementedException())
+    member this.Retrieve(entityName: string, id: Guid, columnSet: ColumnSet): Entity = 
+      raise (System.NotImplementedException())
+    member this.RetrieveMultiple(query: QueryBase): EntityCollection = 
+      raise (System.NotImplementedException())
+    member this.Update(entity: Entity): unit = 
+      raise (System.NotImplementedException())
+    member this.Execute( req: OrganizationRequest ) = 
+      match req with
+      | :? UpdateRequest -> 
+        printfn "test"
+        UpdateResponse() :> OrganizationResponse
+      | :? Messages.WhoAmIRequest -> 
+        let resp = Messages.WhoAmIResponse()
+        resp.Results.Add("UserId", Guid.NewGuid())
+        Messages.WhoAmIResponse() :> OrganizationResponse
+      | _ -> 
+        raise (System.NotImplementedException())
+
+let service = TestOrg() :> IOrganizationService
+
+//[<Fact>]
+//let GetStatusRequest() =
+//  Solution.Import()
+//    //let id = whoAmI service
+//    Assert.NotNull id
---
+++ src/Delegate.Daxif.Test/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+#load "Library1.fs"
+open Delegate.Daxif.Test
+
+// Define your library scripting code here
+
